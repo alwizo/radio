78c3aa9cfd17801e83dec4365061237a32d20dce
Alex Vilchanski <alwizo.com@gmail.com>
2016-09-14 10:05:17 +0200

    gulp-concat installed and added assets/styles/app.less

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 "use strict";
 
 var gulp = require('gulp');
+var less = require('gulp-less');
 
 gulp.task('default', ['styles']);
 
 gulp.task('styles', function () {
-    
+    return gulp.src([
+        'assets/styles/app.less'
+    ])
+        .pipe(less());
 });
