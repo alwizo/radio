5be74f72e0061baa0b682c2a93955415a5e588c3
Alex Vilchanski <alwizo.com@gmail.com>
2016-09-14 11:21:23 +0200

    Now gulp watch for .less files, minify them from source and put them in pulblic/css if they are changed

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 
 var gulp = require('gulp');
 var less = require('gulp-less');
+var concat = require('gulp-concat');
 
 gulp.task('default', ['styles']);
 
@@ -9,5 +10,15 @@ gulp.task('styles', function () {
     return gulp.src([
         'assets/styles/app.less'
     ])
-        .pipe(less());
+        .pipe(less({
+            paths: [
+                'bower_components/bootstrap/less'
+            ]
+        }))
+        .pipe(concat('app.css'))
+        .pipe(gulp.dest('public/css'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('assets/styles/**/*.less', ['styles']);
 });
